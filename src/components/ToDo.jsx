4cc3755222c3346ebe66510489cfa38ed36af649
RostNshka <devkotder@ya.ru>
2026-02-28 13:16:23 +0400

    add components ToDo

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,25 @@
+import AddTaskForm from './AddTaskForm'
+import SearchTaskForm from './SearchTaskForm'
+import ToDoInfo from './ToDoInfo'
+import ToDoList from './ToDoList'
+
+const ToDo = () => {
+  const tasks = [
+    { id: 'task-1', title: 'Покормить кота', isDone: false },
+    { id: 'task-2', title: 'Купить молоко', isDone: true },
+  ]
+  return (
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <AddTaskForm />
+      <SearchTaskForm />
+      <ToDoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone }) => isDone).length}
+      />
+      <ToDoList tasks={tasks} />
+    </div>
+  )
+}
+
+export default ToDo
