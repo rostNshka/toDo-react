b945ba693619ba9bcbbb0580084eef6a6786e371
RostNshka <devkotder@ya.ru>
2026-02-28 13:16:16 +0400

    add components ToDoList

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,29 @@
+import ToDoItem from './ToDoItem'
+
+const ToDoList = (props) => {
+  const {
+    tasks = [],
+  } = props
+
+  const hasTasks = true
+
+  if (!hasTasks) {
+    return <div className="todo__empty-message"></div>
+  }
+
+  return (
+    <ul className="todo__list">
+      {tasks.map((task) => (
+          <ToDoItem
+            className='todo__item'
+            key={task.id}
+            {...task}
+          />
+        )
+      )}
+    </ul>
+  )
+}
+
+export default ToDoList
+
