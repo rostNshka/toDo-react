0b3e0b0daa31216c6f66b645d17fd74f199a32a4
RostNshka <devkotder@ya.ru>
2026-02-28 13:15:12 +0400

    add components AddTaskForm

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,21 @@
+import Field from './Field'
+import Button from './Button'
+
+const AddTaskForm = () => {
+  return (
+    <form className="todo__form">
+      <Field
+        className='todo__field'
+        label='New Task Title'
+        id='new-task'
+      />
+      <Button
+        type='submit'
+      >
+        Add
+      </Button>
+    </form>
+  )
+}
+
+export default AddTaskForm
