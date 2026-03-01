b84a63dc1b83eca77239dd6939c1acbfb8020245
RostNshka <devkotder@ya.ru>
2026-03-01 19:07:13 +0400

    add useState hooks, events function, add logic for delete task, delete all tasks, add task and toggle checkbox

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,22 +2,75 @@ import AddTaskForm from './AddTaskForm'
 import SearchTaskForm from './SearchTaskForm'
 import ToDoInfo from './ToDoInfo'
 import ToDoList from './ToDoList'
+import { useState } from 'react'
 
 const ToDo = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { id: 'task-1', title: 'Покормить кота', isDone: false },
     { id: 'task-2', title: 'Купить молоко', isDone: true },
-  ]
+  ])
+  const[newTaskTitle, setNewTaskTitle] = useState('')
+
+  const deleteAllTasks = () => {
+    const isConfirmed = confirm('Вы действительно хотите удалить все задачи?')
+
+    if (isConfirmed) {
+      return setTasks([])
+    }
+  }
+
+  const deleteTask = (taskId) => {
+    setTasks(
+      tasks.filter((task) => task.id !== taskId)
+    )
+  }
+
+  const toggleTaskComplete = (taskId, isDone) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return {...task, isDone}
+        }
+
+        return task
+      })
+    )
+  }
+
+  const filterTasks = (query) => {}
+
+  const addTask = () => {
+    if (newTaskTitle.trim().length > 0) {
+      const newTask = {
+        id: crypto?.randomUUID() ?? Date.now().toString(),
+        title: newTaskTitle,
+        isDone: false,
+      }
+
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('')
+    }
+  }
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm />
-      <SearchTaskForm />
+      <AddTaskForm
+        addTask={addTask}
+        newTaskTitle={newTaskTitle}
+        setNewTaskTitle={setNewTaskTitle}
+      />
+      <SearchTaskForm onSearchInput={filterTasks}/>
       <ToDoInfo
         total={tasks.length}
         done={tasks.filter(({ isDone }) => isDone).length}
+        onDeleteAllButtonClick={deleteAllTasks}
       />
-      <ToDoList tasks={tasks} />
+      <ToDoList
+        tasks={tasks}
+        onDeleteTaskButtonClick={deleteTask}
+        onTaskCompleteChange={toggleTaskComplete}
+      />
     </div>
   )
 }
