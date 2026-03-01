275e6152b46b0f5e85d39614f9eee7801f9f1af6
RostNshka <devkotder@ya.ru>
2026-03-01 19:10:33 +0400

    add prop for input and add function for delete task

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -4,6 +4,8 @@ const ToDoItem = (props) => {
     id,
     title,
     isDone,
+    onDeleteTaskButtonClick,
+    onTaskCompleteChange,
   } = props
 
   return (
@@ -13,7 +15,7 @@ const ToDoItem = (props) => {
         id={id}
         type="checkbox"
         checked={isDone}
-        readOnly
+        onChange={(event) => onTaskCompleteChange(id, event.target.checked)}
       />
       <label
         className="todo-item__label"
@@ -25,6 +27,7 @@ const ToDoItem = (props) => {
         className="todo-item__delete-button"
         aria-label="Delete"
         title="Delete"
+        onClick={() => onDeleteTaskButtonClick(id)}
       >
         <svg
           width="20"
