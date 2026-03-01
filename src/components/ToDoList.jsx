ea80de09edc2d209cdf4a69a1b4ff3999428f415
RostNshka <devkotder@ya.ru>
2026-03-01 22:05:01 +0400

    add filter and change outcomes when program use message about empty

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,19 +3,25 @@ import ToDoItem from './ToDoItem'
 const ToDoList = (props) => {
   const {
     tasks = [],
+    filteredTasks,
     onDeleteTaskButtonClick,
     onTaskCompleteChange,
   } = props
 
-  const hasTasks = true
+  const hasTasks = tasks.length > 0
+  const isEmptyFilteredTasks = filteredTasks?.length > 0
 
   if (!hasTasks) {
-    return <div className="todo__empty-message"></div>
+    return <div className="todo__empty-message">Задач пока нет</div>
+  }
+
+  if (hasTasks && isEmptyFilteredTasks) {
+    return <div className="todo__empty-message">Задачи не найдены</div>
   }
 
   return (
     <ul className="todo__list">
-      {tasks.map((task) => (
+      {(filteredTasks ?? tasks).map((task) => (
           <ToDoItem
             className='todo__item'
             key={task.id}
