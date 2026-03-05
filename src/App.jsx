fbe344c0ab0877be92f5d006eaca423af656fe45
RostNshka <devkotder@ya.ru>
2026-03-05 21:26:32 +0400

    create routes for Router our project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import ToDo from './components/ToDo'
-import { TasksProvider } from './context/TasksContext'
+import Router from './Router'
+import TasksPage from './pages/TasksPage'
+import TaskPage from './pages/TaskPage'
 
 const App = () => {
+  const routes = {
+    '/': TasksPage,
+    '/tasks/:id': TaskPage,
+    '*': () => <div>404 Page not found</div>,
+  }
+
   return (
-    <TasksProvider>
-      <ToDo />
-    </TasksProvider>
+    <Router routes={routes} />
   )
 }
 
